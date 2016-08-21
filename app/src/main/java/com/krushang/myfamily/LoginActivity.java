4fb26ebfa12a665836e45818a6234e2711829384
mitali sonar <mitali sonar>
2016-08-21 20:37:33 +0530

    Initial commit

---
+++ app/src/main/java/com/krushang/myfamily/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.krushang.myfamily;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MITALI on 10-07-2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
